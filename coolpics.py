1384d0c3835f64f0aa969a80865c352260b1b83e
olivertemple <70803502+olivertemple@users.noreply.github.com>
2021-01-10 16:03:56 +0000

    3D plotting of original shape working in all quadrants

--- coolpics.py
+++ coolpics.py
@@ -43,6 +43,13 @@ lines = []
 intersect = []
 
 def original(num, i, w,setcolor):
+    if num>0:
+        plt.plot([0,0],[0,i-1],color=setcolor, linewidth=w)
+        plt.plot([0,i-1],[0,0],color=setcolor, linewidth=w)
+    if num>1:
+        plt.plot([0,0],[0,-i+1],color=setcolor, linewidth=w)
+    if num>2:
+        plt.plot([0,-i+1],[0,0],color=setcolor, linewidth=w)
     for x in range(1,i):
         if num>0:
             plt.plot([0,i-x],[x,0], color=setcolor, linewidth=w)
@@ -54,6 +61,14 @@ def original(num, i, w,setcolor):
             plt.plot([0,-(i-x)],[-x,0], color=setcolor, linewidth=w)
 
 def triinmiddle(num, i, w,setcolor):
+    if num>0:
+        plt.plot([0,0],[0,i-1],color=setcolor, linewidth=w)
+        plt.plot([0,i-1],[0,0],color=setcolor, linewidth=w)
+    if num>1:
+        plt.plot([0,0],[0,-i+1],color=setcolor, linewidth=w)
+    if num>2:
+        plt.plot([0,-i+1],[0,0],color=setcolor, linewidth=w)
+
     for x in range(1,i):
         if num >0:
             plt.plot([0,i-x],[i-x,x], color=setcolor,linewidth = w)
@@ -75,6 +90,14 @@ def triinmiddle(num, i, w,setcolor):
             plt.plot([-x,-(i-x)],[0,-x], color=setcolor,linewidth = w)
 
 def shape3(num,i,w,setcolor):
+    if num>0:
+        plt.plot([0,0],[0,i-1],color=setcolor, linewidth=w)
+        plt.plot([0,i-1],[0,0],color=setcolor, linewidth=w)
+    if num>1:
+        plt.plot([0,0],[0,-i+1],color=setcolor, linewidth=w)
+    if num>2:
+        plt.plot([0,-i+1],[0,0],color=setcolor, linewidth=w)
+
     for x in range(1,i):
         if num>0:
             plt.plot([0,i-x],[x,0], color=setcolor, linewidth=w)
@@ -97,25 +120,64 @@ def shape3(num,i,w,setcolor):
 def threedimensional1(i):
     fig = plt.figure()
     ax = plt.axes(projection='3d')
+    if num>0:
+        ax.plot([0,0,0],[0,i-1,0],color=setcolor, linewidth=w)
+        ax.plot([0,0],[0,0],[0,i-1],color=setcolor, linewidth=w)        
+        ax.plot([0,i-1,0],[0,0,0],color=setcolor, linewidth=w)
+    if num>1:
+        ax.plot([0,0],[0,-i+1],color=setcolor, linewidth=w)
+    if num>2:
+        ax.plot([0,-i+1],[0,0],color=setcolor, linewidth=w)
+    if num>3:
+        ax.plot([0,0],[0,0],[0,-(i-1)],color=setcolor, linewidth=w)
+
     for x in range(1,i):
-        ax.plot3D([0,i-x],[x,0],[0,0])
-        ax.plot3D([0,0],[x,0],[0,i-x])
-        ax.plot3D([x,0],[0,0],[0,i-x])
+        if num >0:
+            ax.plot3D([0,i-x],[x,0],[0,0], color=setcolor, linewidth=w)
+            ax.plot3D([0,0],[x,0],[0,i-x], color=setcolor, linewidth=w)
+            ax.plot3D([x,0],[0,0],[0,i-x], color=setcolor, linewidth=w)
+        
+        if num>1:
+            ax.plot3D([0,i-x],[-x,0],[0,0], color=setcolor, linewidth=w)
+            ax.plot3D([0,0],[-x,0],[0,i-x], color=setcolor, linewidth=w)
+            ax.plot3D([x,0],[0,0],[0,i-x], color=setcolor, linewidth=w)
+
+        if num>2: 
+            ax.plot3D([0,-(i-x)],[x,0],[0,0], color=setcolor, linewidth=w)
+            ax.plot3D([0,0],[x,0],[0,i-x], color=setcolor, linewidth=w)
+            ax.plot3D([-x,0],[0,0],[0,i-x], color=setcolor, linewidth=w)
+        
+            ax.plot3D([0,-(i-x)],[-x,0],[0,0], color=setcolor, linewidth=w)
+            ax.plot3D([0,0],[-x,0],[0,i-x], color=setcolor, linewidth=w)
+            ax.plot3D([-x,0],[0,0],[0,i-x], color=setcolor, linewidth=w)
+
+        if num>3:
+            ax.plot3D([0,i-x],[x,0],[0,0], color=setcolor, linewidth=w)
+            ax.plot3D([0,0],[x,0],[0,-(i-x)], color=setcolor, linewidth=w)
+            ax.plot3D([x,0],[0,0],[0,-(i-x)], color=setcolor, linewidth=w)
+
+            ax.plot3D([0,i-x],[-x,0],[0,0], color=setcolor, linewidth=w)
+            ax.plot3D([0,0],[-x,0],[0,-(i-x)], color=setcolor, linewidth=w)
+            ax.plot3D([x,0],[0,0],[0,-(i-x)], color=setcolor, linewidth=w)
+
+            ax.plot3D([0,-(i-x)],[x,0],[0,0], color=setcolor, linewidth=w)
+            ax.plot3D([0,0],[x,0],[0,-(i-x)], color=setcolor, linewidth=w)
+            ax.plot3D([-x,0],[0,0],[0,-(i-x)], color=setcolor, linewidth=w)
+
+            ax.plot3D([0,-(i-x)],[-x,0],[0,0], color=setcolor, linewidth=w)
+            ax.plot3D([0,0],[-x,0],[0,-(i-x)], color=setcolor, linewidth=w)
+            ax.plot3D([-x,0],[0,0],[0,-(i-x)], color=setcolor, linewidth=w)
+
+
 
 
 
 
 i = 20
 w=0.5
-num = 1
+num = 4
 setcolor = ('black')
-#if num>0:
-#    plt.plot([0,0],[0,i-1],color=setcolor, linewidth=w)
-#    plt.plot([0,i-1],[0,0],color=setcolor, linewidth=w)
-#if num>1:
-#    plt.plot([0,0],[0,-i+1],color=setcolor, linewidth=w)
-#if num>2:
-#    plt.plot([0,-i+1],[0,0],color=setcolor, linewidth=w)
+
 
 #triinmiddle(num,i,w,"red")
 #original(num,i,w,"blue")
