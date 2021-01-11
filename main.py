c39a7d8569e012b52b4273e3a539be8f8bbd5d7e
olivertemple <70803502+olivertemple@users.noreply.github.com>
2021-01-11 16:36:29 +0000

    Update main.py
    GUI functional, with all inputs operational.

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ def shape3(num,i,w,setcolor):#3rd shape
             plt.plot([-(x-1),-(i-x)],[-(i-x),0], color=setcolor, linewidth=w)
             plt.plot([-(i-x),0],[-(x-1),-(i-x)], color=setcolor, linewidth=w)
 
-def threedimensionaloriginal(num,i,w,setcolor,ax):#3D version of original shape
+def threedimensionaloriginal(num,i,w,setcolor):#3D version of original shape
     #one quadrant
     if num>0:
         plt.plot([0,0,0],[0,i-1,0],color=setcolor, linewidth=w)
@@ -159,7 +159,7 @@ def threedimensionaloriginal(num,i,w,setcolor,ax):#3D version of original shape
             plt.plot([0,0],[-x,0],[0,-(i-x)], color=setcolor, linewidth=w)
             plt.plot([-x,0],[0,0],[0,-(i-x)], color=setcolor, linewidth=w)
 
-def threedimensionaltriinmiddle(num,i,w,setcolor,ax):#3D version of 2nd shape
+def threedimensionaltriinmiddle(num,i,w,setcolor):#3D version of 2nd shape
     #one quadrant
     if num>0:
         plt.plot([0,0,0],[0,i-1,0],color=setcolor, linewidth=w)
@@ -278,7 +278,7 @@ def threedimensionaltriinmiddle(num,i,w,setcolor,ax):#3D version of 2nd shape
             plt.plot([0,-x],[0,0],[-(i-x),0], color=setcolor,linewidth = w)
             plt.plot([-x,-(i-x)],[0,0],[0,-x], color=setcolor,linewidth = w)
 
-def threedimensionalshape3(num,i,w,setcolor,ax):#3D version of the 3rd shape
+def threedimensionalshape3(num,i,w,setcolor):#3D version of the 3rd shape
     #one quadrant
     if num>0:
         plt.plot([0,0,0],[0,i-1,0],color=setcolor, linewidth=w)
@@ -491,37 +491,98 @@ class Toolbar(NavigationToolbar2Tk):
 
 
 menu_def = [['&file',['&save    Ctrl-S']]]
-selections = [
-[sg.Button( image_filename="./outputs/2D, 20 ,1.png", image_subsample=2)],
-[sg.Button(image_filename="./outputs/2D, 20 ,2.png", image_subsample=2)],
-[sg.Button(image_filename="./outputs/2D, 20 ,3.png", image_subsample=2)],
-]
 
 layout = [
 [sg.Text("Geometrical shape generator")],
 [sg.Text("This program will generate either 2D or 3D geometrical shapes, depending on your input.")],
-[sg.Menu(menu_def)],   
-[sg.Button("Run")],
-[sg.Button("Close")],
-[sg.Button("Plot")],
-[sg.Canvas(key='fig_cv', size=(800,800))],
-[sg.Canvas(key='controls_cv')],
-#[sg.Checkbox(text='test')],
-#[sg.Button( image_filename="./outputs/2D, 20 ,1.png", image_subsample=2)],
-#[sg.Button(image_filename="./outputs/2D, 20 ,2.png", image_subsample=2)],
-#[sg.Button(image_filename="./outputs/2D, 20 ,3.png", image_subsample=2)]
+[sg.Menu(menu_def)],
+[sg.Button("1",image_filename="./outputs/2D, 20 ,1.png", image_subsample=2, key='original'), sg.Button("2",image_filename="./outputs/2D, 20 ,2.png", image_subsample=2,key='triinmiddle'), sg.Button("3",image_filename="./outputs/2D, 20 ,3.png", image_subsample=2, key='shape3')],
+[sg.Checkbox('Dimensions, Tick for 3D, leave for 2D', default=False, auto_size_text=True, key='dim')],
+[sg.Text(text='The Shape being Plotted:\n',key='text_box', size = (100,2),pad=(0,0))],
+[sg.Text(text="Colour, in either Hex (#AABBCC), RGB(255,255,255) or as a word (red):",pad=(0,0)),sg.Input(default_text='black', key='setcolor')],
+[sg.Text(text='Size of image:',pad=(0,0)),sg.Input(default_text='20', key='i', size = (5,10)), sg.Text(text="Number of quadrants(1-4):", pad=(0,0)), sg.Input(default_text="4", key="num", size=(5,10)), sg.Text(text="Width of lines (only needs adjusting for large sizes", pad=(0,0)),sg.Input(default_text="0.5", key = 'w', size = (5,10))],
+[sg.Button("Plot"),sg.Button("Reset")],
+[sg.Canvas(key='fig_cv', size=(550,550))],
+[sg.Canvas(key='controls_cv')]
 ]
 
-window= sg.Window("Geometrical Shape Generator", layout, resizable=True)
+window= sg.Window("Geometrical Shape Generator", layout, resizable=True).finalize()
+window.Maximize()
 
-
+shape = 1
+string = 'The Shape being Plotted:\n'
+d2 = False
+d3 = False
 while True:
     event, values = window.read()
     if event=='Close' or event == sg.WIN_CLOSED:
         break
-    elif event == 'Plot':
-        ax = plt.axes(projection='3d')
-        threedimensionaloriginal(4,20,0.5,'black',ax)
+    if event=='original':
+        shape=1
+    elif event=='triinmiddle':
+        shape=2
+    elif event=='shape3':
+        shape = 3
+
+    if values['dim']==True:
+        dim = 3
+        if d3 == False:
+            ax = plt.axes(projection='3d')
+            string = 'The Shape being Plotted:\n'
+            window.Element('text_box').update(string)
+        d3 = True
+        d2 = False
+    elif values['dim']==False:
+        dim = 2
+        if d2 == False:
+            ax = plt.axes()
+            string = 'The Shape being Plotted:\n'
+            window.Element('text_box').update(string)
+        d2 = True
+        d3 = False
+    if event == 'Reset':
+        string = 'The Shape being Plotted:\n'
+        window.Element('text_box').update(string)
+        if dim == 2:
+            plt.cla()
+            plt.figure(1)
+            fig = plt.gcf()
+            DPI=fig.get_dpi()
+            fig.set_size_inches(404*2/float(DPI),404/float(DPI))
+            draw_figure_w_toolbar(window['fig_cv'].TKCanvas, fig, window['controls_cv'].TKCanvas)
+        elif dim==3:
+            plt.cla()
+            plt.figure(1)
+            fig = plt.gcf()
+            DPI=fig.get_dpi()
+            fig.set_size_inches(404*2/float(DPI),404/float(DPI))
+            draw_figure_w_toolbar(window['fig_cv'].TKCanvas, fig, window['controls_cv'].TKCanvas)
+    else:
+        window.Element('text_box').update(string+' Shape' + str(shape)+', '+str(dim)+'D.')
+
+    if event == 'Plot': 
+        string += (' Shape' + str(shape)+', '+str(dim)+'D.')
+        window.Element('text_box').update(string)      
+        setcolor = (values['setcolor'])
+        i = int(values["i"])
+        num = int(values["num"])
+        w = float(values['w'])
+        if dim == 2:
+            #ax = plt.axes()
+            if shape == 1:
+                original(num, i, w, setcolor)
+            elif shape==2:
+                triinmiddle(num, i, w, setcolor)
+            elif shape==3:
+                shape3(num, i, w,setcolor)
+        elif dim==3:
+            #ax = plt.axes(projection='3d')
+            if shape == 1:
+                threedimensionaloriginal(num, i, w, setcolor)
+            elif shape==2:
+                threedimensionaltriinmiddle(num, i, w, setcolor)
+            elif shape==2:
+                threedimensionalshape3(num, i, w , setcolor)
 
         plt.figure(1)
         fig = plt.gcf()
@@ -529,7 +590,6 @@ while True:
 
         fig.set_size_inches(404*2/float(DPI),404/float(DPI))
         draw_figure_w_toolbar(window['fig_cv'].TKCanvas, fig, window['controls_cv'].TKCanvas)
-
 
 
 
